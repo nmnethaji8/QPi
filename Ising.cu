ed23f5ef251c9d5ec3c7200def94f8f16835f132
nmnethaji8 <nmnethaji8@gmail.com>
2022-05-28 15:18:31 +0530

    Calculating Total Energy of the system

--- Ising.cu
+++ Ising.cu
@@ -2,10 +2,9 @@
 
 using namespace std;
 
-double System_Energy(int *lattice, int N)
+double System_Energy(int *lattice, int const N)
 {
-   double Energy=0,a,b,c,d;
-   int lattice2[N*N];
+   double Energy=0,GlobalEnergy=0;
    for(int i=0; i<N; i++)
    {
       for(int j=0; j<N; j++)
@@ -13,53 +12,44 @@ double System_Energy(int *lattice, int N)
          if((i!=0)&&(i!=N-1)&&(j!=0)&&(j!=N-1))
          {
             Energy=lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[(i-1)*N+j]+lattice[i*N+j-1];
-            cout << lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[(i-1)*N+j]+lattice[i*N+j-1] << "\t";
          }
          if((i==0)&&(i!=N-1)&&(j!=0)&&(j!=N-1))
          {
             Energy=lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[i*N+j-1];
-            cout << lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[i*N+j-1] << "\t";
          }
          if((i!=0)&&(i==N-1)&&(j!=0)&&(j!=N-1))
          {
             Energy=lattice[i*N+j+1]+lattice[(i-1)*N+j]+lattice[i*N+j-1];
-            cout << lattice[i*N+j+1]+lattice[(i-1)*N+j]+lattice[i*N+j-1] << "\t";
          }
          if((i!=0)&&(i!=N-1)&&(j==0)&&(j!=N-1))
          {
             Energy=lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[(i-1)*N+j];
-            cout << lattice[(i+1)*N+j]+lattice[i*N+j+1]+lattice[(i-1)*N+j]<< "\t";
          }
          if((i!=0)&&(i!=N-1)&&(j!=0)&&(j==N-1))
          {
             Energy=lattice[(i+1)*N+j]+lattice[(i-1)*N+j]+lattice[i*N+j-1];
-            cout << lattice[(i+1)*N+j]+lattice[(i-1)*N+j]+lattice[i*N+j-1] << "\t";
          }
          if((i==0)&&(j==0))
          {
             Energy=lattice[(i+1)*N+j]+lattice[i*N+j+1];
-            cout << lattice[(i+1)*N+j]+lattice[i*N+j+1]<< "\t";
          }
          if((i==N-1)&&(j==N-1))
          {
             Energy=lattice[(i-1)*N+j]+lattice[i*N+j-1];
-            cout << lattice[(i-1)*N+j]+lattice[i*N+j-1] << "\t";
          }
          if((i==0)&&(j==N-1))
          {
             Energy=lattice[(i+1)*N+j]+lattice[i*N+j-1];
-            cout << lattice[(i+1)*N+j]+lattice[i*N+j-1] << "\t";
          }
          if((i==N-1)&&(j==0))
          {
             Energy=lattice[i*N+j+1]+lattice[(i-1)*N+j];
-            cout << lattice[i*N+j+1]+lattice[(i-1)*N+j] << "\t";
          }
-         lattice2[i*N+j]=Energy;
+         GlobalEnergy=GlobalEnergy+(-lattice[i*N+j]*Energy);
       }
-      cout << "\n";
    }
-   return Energy;
+
+   return GlobalEnergy;
 }
 
 int main()
@@ -68,7 +58,7 @@ int main()
    int lattice_p[]={ 1,  1,  1,  1,  1, -1,  1, 1,  1,  1, 1,  1,  1,  1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1, -1};
    int N=5;
 
-   cout << "Energy of System is \n"<< System_Energy(lattice_p,N) << endl;
+   cout << "Energy of System is "<< System_Energy(lattice_p,N) << endl;
 
    return 0;
 }
