7e422326ff98f1f377a3626a5ddf86beea3730a6
nmnethaji8 <nmnethaji8@gmail.com>
2022-05-30 11:56:26 +0530

    Using constructor in edges

--- GraphsWithIsing.cu
+++ GraphsWithIsing.cu
@@ -4,10 +4,26 @@
 
 using namespace std;
 
+#define cMM cudaMallocManaged
+
 class Edge
 {
    public:
-   int v0=0,v1=0,wt=0;
+   int v0,v1,wt;
+   Edge()
+   {
+      v0=0,v1=0,wt=0;
+   }
+};
+
+class Vertices
+{
+   public:
+   int *Neigh;
+   Vertices()
+   {
+      Neigh=nullptr;
+   }
 };
 
 int main()
@@ -20,9 +36,11 @@ int main()
 
    Edge edges[E];
 
+   //int i= cMM(&edges, E*sizeof(Edge));
    for(int i=0;i<E;i++)
    {
       Graph >> edges[i].v0 >> edges[i].v1 >> edges[i].wt;
+      cout << edges[i].v0 << " " << edges[i].v1 << " " << edges[i].wt<< "\n" ;
    }
    return 0;
 }
